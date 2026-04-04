e4e8a9d2ca696de162217cd9dc0baab077aea82d
lalit <easypoolesp@gmail.com>
2026-04-04 21:15:59 +0530

    Use robust monkeypatch for Cloud SQL connector

--- ssa_alumni/settings/prod.py
+++ ssa_alumni/settings/prod.py
@@ -21,9 +21,22 @@ SECURE_SSL_REDIRECT     = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # --- Cloud SQL IAM Authentication ---
+# Initialize Cloud SQL Connector
+connector = Connector()
+
+def getconn():
+    return connector.connect(
+        os.environ.get("DB_HOST").replace("/cloudsql/", ""), # Connection name
+        "pg8000",
+        user=os.environ.get("DB_USER"),
+        db=os.environ.get("DB_NAME"),
+        enable_iam_auth=True,
+        ip_type=IPTypes.PUBLIC
+    )
+
 DATABASES = {
     "default": {
-        "ENGINE": "ssa_alumni.db_engine", # Use our custom engine
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": "",
@@ -32,6 +45,15 @@ DATABASES = {
         "CONN_MAX_AGE": 600,
     }
 }
+
+# Robust monkeypatch: Intercept the connection before Django tries to use psycopg2
+from django.db.backends.postgresql.base import DatabaseWrapper
+
+def patched_get_new_connection(self, conn_params):
+    return getconn()
+
+DatabaseWrapper.get_new_connection = patched_get_new_connection
+
 
 
 
